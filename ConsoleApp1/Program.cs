645e798e6de7310d5b8f5693d2d6093ce7075c2e
300369419 <300369419@N6107S37>
2023-02-10 11:26:01 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Real state app
+            //Read-only Address (string), year built (int) and purchase price (double)
+            //Read-write IncomeFromRent (double) and monthlyExpense (double)
+            //Read-only computed MonthlyEarnings (double) and IncomeFromRent (dobule = IncomeFromRent - MonthlyExpenses)
+            // Add constructor -> ToString()
+
+            //with new I create an object building1
+            RealEstate building1 = new RealEstate("328 Agnes", 1999, 900000);
+            building1.IncomeFromRent = 1200.99;
+            building1.MonthlyExpense = 289.98;
+            WriteLine(building1);
+
+            GetAdressYearAndPrice(out string address, out int year, out double price);
+            RealEstate building2 = new RealEstate(address, year, price);
+            WriteLine(building2.ToString());
+
+            LoadIncomeAndExpenses(building2);
+            WriteLine(building2);
+        }
+
+        static void LoadIncomeAndExpenses(RealEstate anybuilding)
+        {
+            //get user input for income and expese
+            Write("Enter the income from rent for " + anybuilding.Address + ": ");
+            anybuilding.IncomeFromRent = double.Parse(ReadLine());
+            Write("Enter the expense for {0}: ", anybuilding.Address);
+            anybuilding.MonthlyExpense = double.Parse(ReadLine());
+            //set anybuilding monthly expense
+
+        }
+        //method and variables oput to be able to use it outside
+        static void GetAdressYearAndPrice(out string address, out int year, out double price)
+        {
+            Write("Enter the address: ");
+            address = ReadLine();
+            Write("Enter the year built: ");
+            year = int.Parse(ReadLine());
+            Write("Enter the purchase price: ");
+            price = double.Parse(ReadLine());
+        }
+    }
+}
---
+++ ConsoleApp1/RealEstate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class RealEstate
+    {
+        public string Address { get; }
+        public int YearBuilt { get; }
+        public double PurchasePrice 
+        { 
+            get; 
+        }
+        public double IncomeFromRent { get; set; }
+        public double MonthlyExpense { get; set; }
+        public double MonthlyEarnings //read only computed
+        {
+            get 
+            { 
+                return IncomeFromRent - MonthlyExpense; 
+            }
+        }
+
+        //alternate computation using method
+        public double GetMonthlyEarnings()
+        {
+            return IncomeFromRent - MonthlyExpense;
+        }
+
+        //constructor
+        public RealEstate(string address, int yearBuilt, double puchasePrice) //same name as the class
+        { 
+            Address = address; 
+            YearBuilt = yearBuilt;
+            PurchasePrice = puchasePrice;
+        }
+
+        public override string ToString()
+        {
+            string outputStr = "Address: " + Address + "\n" +
+                               "Year Built: " + YearBuilt + "\n" +
+                               "Purchase Price: " + PurchasePrice.ToString("C") + "\n" +
+                               "Income from rent: " + IncomeFromRent.ToString("C") + "\n" +
+                               "Monthly Expenses: " + MonthlyExpense.ToString("C") + "\n" +
+                               "Monthly Earnings: " + MonthlyEarnings.ToString("C") + "\n";
+            return outputStr;
+        }
+    }
+}
